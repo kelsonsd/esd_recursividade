9602350eb37b752fa203acdce161ce1e862d41a5
kelsonsd <kelsonsd@gmail.com>
2013-05-23 10:59:41 -0300

    Update SomaRecursiva.java

--- SomaRecursiva.java
+++ SomaRecursiva.java
@@ -1,15 +1,22 @@
-    public class SomaRecursiva {  
-      
-        public static void main(String[] args) {  
-            int vetor[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };  
-            System.out.println("soma: " + somaVetor(vetor, vetor.length));  
-        }  
-      
-        public static int somaVetor(int vetor[], int tamanho) {  
-            if (tamanho > 0) {  
-                return vetor[tamanho - 1] + somaVetor(vetor, tamanho - 1);  
-            } else {  
-                return 0;  
-            }  
-        }  
-    }  
+public class SomaRecursiva {
+
+    public static void main(String[] args) {
+
+		int[] vetor = new int[args.length];
+		for (int i = 0; i < args.length; i++) {
+			vetor[i] = Integer.parseInt(args[i]);
+
+		}
+
+		System.out.println("Soma: " + somaVetor(vetor, vetor.length));
+
+	}
+
+	public static int somaVetor(int vetor[], int tamanho) {
+		if (tamanho > 0) {
+			return vetor[tamanho - 1] + somaVetor(vetor, tamanho - 1);
+		} else {
+			return 0;
+		}
+	}
+}
