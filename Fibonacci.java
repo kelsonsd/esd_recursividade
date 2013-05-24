60d6a67a2b7f0a8e8132b663c212053116c4b90e
kelsonsd <kelsonsd@gmail.com>
2013-05-24 08:49:05 -0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,6 +6,6 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
-		System.out.println("O Fibonacci de "+ n +"- eh: "+fibonacci(n));
+		System.out.println(fibonacci(n));
 	}
 }
